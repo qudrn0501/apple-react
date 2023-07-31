bf51a0fb6c53f3554661a47018cd70ccdc5e7b2a
qudrn0501 <104612845+qudrn0501@users.noreply.github.com>
2023-07-31 14:08:22 +0900

    중요한 데이터는 변수말고 state에 담습니다

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import './App.css';
-// import { useState } from 'react';
+import { useState } from 'react';
 
 function App() {
   let post = '강남 우동 맛집';
+  let [글제목, b] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
   
 
   // 숙제: 
   return (
     <div className='App'>
       <div className='black-nav'>
-        <h4 style={ {color : 'red', fontSize : '20px'}}>블로그임</h4>
+        <h4>블로그임</h4>
       </div>
-      <h4>{post}</h4>
+      <div className='list'>
+        <h4>{글제목[0]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{글제목[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{글제목[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
