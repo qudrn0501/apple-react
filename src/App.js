549d854d48067d4d1c3e97e51d078fef18d6df23
qudrn0501 <104612845+qudrn0501@users.noreply.github.com>
2023-08-22 18:27:11 +0900

    map : 많은 div들을 반복문으로 줄이고 싶은 충동이 들 때

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import { useState } from 'react';
 function App() {
   let post = '강남 우동 맛집';
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
-  let [따봉, 따봉변경] = useState(0);
+  let [따봉, 따봉변경] = useState([0,0,0]);
   let [modal, setModal] = useState(false);
 
 
-  // 숙제: 제목 또 누르면 모달창 사라지게
+  // 숙제: 따봉갯수 개별로 기록하기
   return (
     <div className='App'>
       <div className='black-nav'>
         <h4>블로그임</h4>
       </div>
-      <div className='list'>
+      {/* <div className='list'>
         <h4>{글제목[0]} <span onClick={()=>{따봉변경(따봉 + 1)}}>👍</span> {따봉} </h4>
         <p>2월 17일 발행</p> 
         
@@ -26,7 +26,8 @@ function App() {
       <div className='list'>
         <h4 onClick={()=>{ setModal(!modal)}}>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
-      </div>
+      </div> */}
+      
       <button onClick={()=>{
           let copy = [...글제목];
           copy[0] = '여자 코트 추천';
@@ -40,6 +41,22 @@ function App() {
 
       {
         modal == true ? <Modal/> : null
+      }
+      {
+        글제목.map(function(a, i){
+          return (
+            <div className='list'>
+              <h4>{글제목[i]} 
+                <span onClick={()=>{
+                  let copy따봉 = [...따봉];
+                  copy따봉[i] = copy따봉[i] + 1;
+                  따봉변경(copy따봉);
+                }}>👍</span> {따봉[i]} 
+              </h4>
+              <p>2월 17일 발행</p> 
+            </div> 
+          )
+        })
       }
     </div>
   );
