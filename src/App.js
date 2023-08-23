9af926b7de174896e8a9c765328e6e0da1573d22
qudrn0501 <104612845+qudrn0501@users.noreply.github.com>
2023-08-23 18:39:12 +0900

    자식이 부모의 state 가져다쓰고 싶을 때는 props

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  let post = '강남 우동 맛집';
+  // let post = '강남 우동 맛집';
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
   let [따봉, 따봉변경] = useState([0,0,0]);
   let [modal, setModal] = useState(false);
 
 
-  // 숙제: 따봉갯수 개별로 기록하기
+  // 숙제: 글수정 버튼누르면 첫 글제목이 '여자코트 추천'으로 바뀌어야함
   return (
     <div className='App'>
       <div className='black-nav'>
@@ -39,14 +39,12 @@ function App() {
           글제목변경(array)
       }}>가나다순 정렬</button>
 
-      {
-        modal == true ? <Modal/> : null
-      }
+
       {
         글제목.map(function(a, i){
           return (
-            <div className='list'>
-              <h4>{글제목[i]} 
+            <div className='list' key={i}>
+              <h4 onClick={()=>{setModal(!modal)}}>{글제목[i]} 
                 <span onClick={()=>{
                   let copy따봉 = [...따봉];
                   copy따봉[i] = copy따봉[i] + 1;
@@ -58,16 +56,22 @@ function App() {
           )
         })
       }
+
+      {
+        modal === true ? <Modal color={'orange'} 글제목 = {글제목} 글제목변경 = {글제목변경}/> : null
+      }
     </div>
   );
 }
 
-function Modal(){
+function Modal(props){
   return (
-    <div className='modal'>
-      <h4>제목</h4>
+    <div className='modal' style={{background : props.color}}>
+      <h4>{props.글제목[0]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+
+      <button onClick={()=>{props.글제목변경(['여자코트 추천', '강남 우동맛집', '파이썬독학'])}}>글제목수정</button>
     </div>
   )
 }
