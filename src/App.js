6bb08c8ef5deea60917849dd839d9ac236a66445
qudrn0501 <104612845+qudrn0501@users.noreply.github.com>
2023-08-24 15:20:03 +0900

    input 1 : 사용자가 입력한 글 다루기

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ function App() {
   let [따봉, 따봉변경] = useState([0,0,0]);
   let [modal, setModal] = useState(false);
   let [title, setTitle] = useState(0);
+  let [입력값, 입력값변경] = useState('');
 
 
-  // 숙제: 글수정 버튼누르면 첫 글제목이 '여자코트 추천'으로 바뀌어야함
+  // 숙제: 버튼누르면 글 하나 추가되는 기능 만들기, 글마다 삭제버튼 & 기능 만들기
   return (
     <div className='App'>
       <div className='black-nav'>
@@ -29,7 +30,7 @@ function App() {
         <p>2월 17일 발행</p>
       </div> */}
       
-      <button onClick={()=>{
+      {/* <button onClick={()=>{
           let copy = [...글제목];
           copy[0] = '여자 코트 추천';
           글제목변경(copy);
@@ -38,29 +39,46 @@ function App() {
           let array = [...글제목];
           array = array.sort();
           글제목변경(array)
-      }}>가나다순 정렬</button>
+      }}>가나다순 정렬</button> */}
 
 
       {
         글제목.map(function(a, i){
           return (
             <div className='list' key={i}>
-              <h4 onClick={()=>{setModal(!modal); setTitle(i)}}>{글제목[i]} 
-                <span onClick={()=>{
+              <h4 onClick={()=>{
+                setModal(!modal); setTitle(i)}}>{글제목[i]} 
+                <span onClick={(e)=>{
+                  e.stopPropagation();
                   let copy따봉 = [...따봉];
                   copy따봉[i] = copy따봉[i] + 1;
                   따봉변경(copy따봉);
                 }}>👍</span> {따봉[i]} 
               </h4>
-              <p>2월 17일 발행</p> 
+              <p>2월 17일 발행</p>
+              <button onClick={()=>{
+                let copy = [...글제목];
+                copy.splice(i, 1);
+                글제목변경(copy);
+              }}>글삭</button> 
             </div> 
           )
         })
       }
 
-      <button onClick={()=>{setTitle(0)}}>글제목0</button>
+      {/* <button onClick={()=>{setTitle(0)}}>글제목0</button>
       <button onClick={()=>{setTitle(1)}}>글제목1</button>
-      <button onClick={()=>{setTitle(2)}}>글제목2</button>
+      <button onClick={()=>{setTitle(2)}}>글제목2</button> */}
+
+      <input onChange={(e)=>{
+        입력값변경(e.target.value)
+        console.log(입력값)
+      }} />
+      <button onClick={()=>{
+        let copy = [...글제목];
+        copy.unshift(입력값);
+        글제목변경(copy)
+      }}>글추가</button>
 
       {
         modal === true ? <Modal title = {title} color={'orange'} 글제목 = {글제목} 글제목변경 = {글제목변경}/> : null
